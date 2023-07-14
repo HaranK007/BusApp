3fbddbcc4dda2feb9dd310cd134c3d1cad098627
HaranK007 <108117611+HaranK007@users.noreply.github.com>
2023-07-14 11:58:53 +0530

    main prototype

--- App.js
+++ App.js
@@ -1,129 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button, Alert, ScrollView, Text, Image } from 'react-native';
-import * as SQLite from 'expo-sqlite';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Onbording from './component/Onbording'
+import Main from './component/Main';
 
-const db = SQLite.openDatabase('BusApp.db');
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
-  const [databaseInitialized, setDatabaseInitialized] = useState(false);
-  const [busList, setBusList] = useState([]);
+const Stack = createStackNavigator();
 
-  useEffect(() => {
-    initializeDatabase();
-  }, []);
-
-  const initializeDatabase = () => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS buses (id INTEGER PRIMARY KEY AUTOINCREMENT, busName TEXT NOT NULL, busSource TEXT NOT NULL, busDestination TEXT NOT NULL)',
-        [],
-        () => {
-          tx.executeSql(
-            'SELECT * FROM buses',
-            [],
-            (_, result) => {
-              if (result.rows.length === 0) {
-                // Insert sample data
-                tx.executeSql('INSERT INTO buses (busName, busSource, busDestination) VALUES (?, ?, ?)', ['Bus 1', 'Source 1', 'Destination 1']);
-                tx.executeSql('INSERT INTO buses (busName, busSource, busDestination) VALUES (?, ?, ?)', ['Bus 2', 'Source 2', 'Destination 2']);
-                tx.executeSql('INSERT INTO buses (busName, busSource, busDestination) VALUES (?, ?, ?)', ['Bus 3', 'Source 3', 'Destination 3']);
-                tx.executeSql('INSERT INTO buses (busName, busSource, busDestination) VALUES (?, ?, ?)', ['Bus 4', 'Source 4', 'Destination 4']);
-                tx.executeSql('INSERT INTO buses (busName, busSource, busDestination) VALUES (?, ?, ?)', ['Bus 5', 'Source 5', 'Destination 5']);
-              }
-            },
-            (_, error) => {
-              Alert.alert('Error checking existing data:', error.message);
-            }
-          );
-          setDatabaseInitialized(true);
-        },
-        (_, error) => {
-          Alert.alert('Error initializing database:', error.message);
-        }
-      );
-    });
-  };
-
-  const searchBus = () => {
-    if (!databaseInitialized) {
-      Alert.alert('Database not initialized');
-      return;
-    }
-
-    db.transaction((tx) => {
-      tx.executeSql(
-        'SELECT * FROM buses WHERE busSource = ? OR busDestination = ?',
-        [from, to],
-        (_, result) => {
-          if (result.rows.length > 0) {
-            const buses = [];
-            for (let i = 0; i < result.rows.length; i++) {
-              const bus = result.rows.item(i);
-              buses.push(bus);
-            }
-            setBusList(buses);
-          } else {
-            setBusList([]);
-            Alert.alert('No buses found');
-          }
-        },
-        (_, error) => {
-          Alert.alert('Error searching for buses:', error.message);
-        }
-      );
-    });
-  };
-
+export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#f1f4ed', padding: 20 }}>
-      <View style={{ alignItems: 'center' }}>
-        {/* <Image
-          source={require('./logo4.jpg')}
-          style={{ width: 200, height: 200, marginBottom: 50 }}
-          resizeMode="contain"
-        /> */}
-      </View>
-      <View style={{ backgroundColor: '#0b1820', padding: 20, borderRadius: 5 }}>
-        <TextInput
-          placeholder="From"
-          value={from}
-          onChangeText={(text) => setFrom(text)}
-          style={{ marginBottom: 10, padding: 10, borderWidth: 1, borderRadius: 5, backgroundColor: '#ffffff' }}
-        />
-        <TextInput
-          placeholder="To"
-          value={to}
-          onChangeText={(text) => setTo(text)}
-          style={{ marginBottom: 10, padding: 10, borderWidth: 1, borderRadius: 5, backgroundColor: '#ffffff' }}
-        />
-        <Button title="Submit" onPress={searchBus} />
-      </View>
+    // <View style={styles.container}>
+    //   <Onbording />
+    //   <StatusBar style="auto" />
+    // </View>
 
-      <ScrollView style={{ flex: 2 }}>
-        {busList.length > 0 ? (
-          busList.map((bus, index) => (
-            <View
-              key={index}
-              style={{
-                borderBottomWidth: 1,
-                borderBottomColor: 'gray',
-                paddingVertical: 10,
-                paddingHorizontal: 20,
-              }}
-            >
-              <Text style={{ fontWeight: 'bold' }}>{bus.busName}</Text>
-              <Text>Source: {bus.busSource}</Text>
-              <Text>Destination: {bus.busDestination}</Text>
-            </View>
-          ))
-        ) : (
-          <Text style={{ textAlign: 'center', marginTop: 20 }}>No buses available</Text>
-        )}
-      </ScrollView>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen  name="Onbording" component={Onbording} />
+        <Stack.Screen  name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
